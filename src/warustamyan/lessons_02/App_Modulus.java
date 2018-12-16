800461ce2e70d4923283952006cd93d3f13784e5
William Arustamyan <williamarustamyan@workfront.com>
2018-12-16 13:26:26 +0400

    warustamyan: modulus example

---
+++ src/warustamyan/lessons_02/App_Modulus.java
@@ -0,0 +1,24 @@
+package warustamyan.lessons_02;
+
+
+public class App_Modulus {
+
+    public static void main(String[] args) {
+
+
+        /*
+        % operatory kiravum e miayn tveri het
+        x % y nshanakum e x-y bajanel y ev vercnel mnacordayin masy
+        orinak 5 % 10 == 5 qni vor 5 / 10 = 0.5 => mnacordayin masy havasar e 5-i amboxj masy 0 e
+         */
+
+        System.out.println(5 % 10);
+
+
+        /*
+        4 % 2 == 0 qni vor 4 / 2 = 2 bajanvum e aranc mnacordi => 0
+         */
+
+        System.out.println(4 % 2);
+    }
+}
