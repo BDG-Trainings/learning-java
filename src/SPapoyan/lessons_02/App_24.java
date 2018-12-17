7bbf598a7787814c512591320963af522dce4079
Sergey Papoyan <spapoyan87@gmail.com>
2018-12-17 19:23:16 +0400

    SPapoyan: App_21

---
+++ src/SPapoyan/lessons_02/App_24.java
@@ -0,0 +1,16 @@
+package SPapoyan.lessons_02;
+
+public class App_24 {
+    public static void main(String[] args) {
+
+        int a = 52;
+        int b = 24;
+        int c = 2;
+        if ((a==2 && b==2) || (a==2 && c==2) || (b==2 && c==2))  {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+    }
+}
