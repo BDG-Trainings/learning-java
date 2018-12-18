7f7839a08a3b8dfbea07d8dda9734909d7ae7783
Anahit Ghazaryan <anahit.ghazaryan91@gmail.com>
2018-12-19 01:50:35 +0400

    Add files via upload

---
+++ App_21.java
@@ -0,0 +1,28 @@
+
+public class App_21 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 15;
+		int c = 25;
+		
+		if (a > b &&  a > c ) {
+			
+		System.out.println(a);
+		}
+		
+		if ( b > a && b > c ) { 
+			System.out.println(b);
+		}
+		if (c > a && c > b ) {
+			System.out.println(c);
+		}
+			
+		else { 
+			System.out.println("There is no correct result ");
+
+		}
+	}
+
+}
