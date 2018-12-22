e47524a79edf693648a5fe80e93bfd3897b1c35e
AramHovnanyan <aramhovnanyan@gmail.com>
2018-12-22 22:34:48 +0400

    Homework exercises for lesson 1

--- src/AramHovnanyan/Homework_03/Exercise_211_230.java
+++ src/AramHovnanyan/Homework_03/Exercise_211_230.java
@@ -1,4 +1,21 @@
 package AramHovnanyan.Homework_03;
 
 public class Exercise_211_230 {
+    public static void main(String[] args) {
+        int [] array = {0,-10,-22,6};
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                sum = sum + array[i];
+                count = count + 1;
+            }
+        }
+        if (count > 0) {
+            double mijtv = (double) sum / count;
+            System.out.println("Mijin tvabanakan drakan" + " - " + mijtv);
+        } else {
+            System.out.println("Mijin tvabanakan drakan" + " - " +  0);
+        }
+    }
 }
---
+++ src/AramHovnanyan/Homework_03/Exercise_254_270.java
@@ -0,0 +1,4 @@
+package AramHovnanyan.Homework_03;
+
+public class Exercise_254_270 {
+}
