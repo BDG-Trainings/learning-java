bf4710f9238689c190bcdf218fb7d268b944e3e2
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2018-12-24 14:10:07 +0400

    Arsen Karapetyan lesson 4

---
+++ src/arsenkarapetyan/lesson_4/App_276.java
@@ -0,0 +1,17 @@
+package arsenkarapetyan.lesson_4;
+
+public class App_276 {
+
+    public static void main(String[] args) {
+
+        boolean t = false;
+        char[] symbols = {'A', 'v', 's', 'a', 'x', 's', 'N', 'v', 'r', 'b', 's', 'm'};
+
+        for (int i = 0; i < symbols.length; i++) {
+            if (symbols[i] == 114) {
+                t = true;
+            }
+        }
+        System.out.println(t);
+    }
+}
