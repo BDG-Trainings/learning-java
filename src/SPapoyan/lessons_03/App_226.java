6a0029e1708eacc0f11e2546588e9d2b5d36481d
Sergey Papoyan <spapoyan87@gmail.com>
2018-12-27 20:22:24 +0400

    SPapoyan

--- src/SPapoyan/lessons_03/App_226.java
+++ src/SPapoyan/lessons_03/App_226.java
@@ -1,18 +1,18 @@
 package SPapoyan.lessons_03;
 
-public class App_225 {
+public class App_226 {
     public static void main (String [] args) {
-        int t = 30;
+        int t = 40;
 
-        int mult = 1;
+        int N = 0;
 
-        int [] numbers = {8, 14, 25, 31, -45, 65};
+        int [] numbers = {8, 14, 25, -31, 45, 65};
 
         for (int i = 0; i < numbers.length; i++) {
             if (Math.abs (numbers [i]) < t ) {
-                mult = mult * numbers [i];
+                N = N + 1;
 
             }
-        }System.out.println(+mult);
+        }System.out.println(+N);
     }
 }
