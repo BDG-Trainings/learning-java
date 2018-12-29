d8437aedae0ee51e21cb5f1eebe092b9da67ccae
Arsen Karapetyan <arsen.karapetyansam@gmail.com>
2018-12-29 14:24:46 +0400

    random

--- src/arsenkarapetyan/lesson_5/App_370.java
+++ src/arsenkarapetyan/lesson_5/App_370.java
@@ -7,16 +7,15 @@ public class App_370 {
     public static void main(String[] args) {
 
         Random num = new Random ();
-
         final int row = 5;
         final int column = 5;
         int [][] array = new int [row][column];
 
         for (int i=0; i<row; i++){
             for (int k = 0; k<row; k++){
-                int a = num.nextInt(100);
+                int a = (num.nextInt(90) + 10);
                 array[i][k]=a;
-                System.err.print(array[i][k]+ ",");
+                System.err.print(array[i][k]+ "\t");
             }System.err.println();
         }
     }
