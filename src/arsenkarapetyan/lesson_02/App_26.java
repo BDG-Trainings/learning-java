482a5fe2157685c75d912880c4697d40ea7e5995
ArsenKarapetyan <arsen.karapetyansam@gmail.com>
2018-12-30 17:07:31 +0400

    update

---
+++ src/arsenkarapetyan/lesson_02/App_26.java
@@ -0,0 +1,15 @@
+package arsenkarapetyan.lesson_02;
+
+public class App_26 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 9;
+        int c = 11;
+        if (a % 2 == 0)
+
+
+        if (b % 2 != 0)
+            if (c % 2 != 0)
+                System.out.println(true);
+    }
+}
