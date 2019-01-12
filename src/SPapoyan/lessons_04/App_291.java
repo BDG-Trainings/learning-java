7b4e55a6a8fe9c6bfadf4f8c22c0dffb8973bca3
Sergey Papoyan <spapoyan87@gmail.com>
2019-01-12 18:31:18 +0400

    SPapoyan

--- src/SPapoyan/lessons_04/App_291.java
+++ src/SPapoyan/lessons_04/App_291.java
@@ -1,19 +1,24 @@
 package SPapoyan.lessons_04;
 
+import java.util.Arrays;
+
 public class App_291 {
     public static void main (String[]args) {
 
         int [] X = {1,2,3,-4,5,-6,-7,8,-9,10};
-        int [] Y = new int[X.length];
         int k = 0;
         for (int i=0; i<X.length; i++) {
             if (X[i] < 0)
-                Y[k] = X[i];
-            k++;
+                k++;
+                //System.out.print(X[i] + ",");
         }
-        for (int i = 0; i < Y.length; i++) {
-
-            System.out.print(Y[i] + ",");
-        }
+            int j = 0;
+        int [] Y = new int[k];
+        for (int i = 0; i < X.length; i++) {
+            if (X[i] < 0) {
+                Y[j] = X[i];
+                j++;
+            }
+        }System.out.println(Arrays.toString(Y));
     }
 }
