658d9d53714770e5b457db1047ab56465fa89677
Vika <vika.karamyan@gmail.com>
2019-01-13 14:53:31 +0400

    Homework

--- src/vkaramyan/lesson_3/App_222.java
+++ src/vkaramyan/lesson_3/App_222.java
@@ -5,6 +5,16 @@ public class App_222 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] numbers = { 1, 12, -5, 8, 6, -4, 7, 9, 2, 3 };
+		int[] range = { 0, 10 };
+		int count = 1;
+
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] >= range[0] && numbers[i] <= range[1]) {
+				count = count * numbers[i];
+			}
+		}
+		System.out.println("Multiply is:" + count);
 	}
 
 }
