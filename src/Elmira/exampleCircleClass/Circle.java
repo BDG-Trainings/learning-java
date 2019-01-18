73d3c9e252893c4ef6d64898a6e7f516eb34cfcd
elmira_k82 <elmirakirakosyan@gmail.com>
2019-01-19 00:03:29 +0400

    Circle Final class

--- src/Elmira/exampleCircleClass/Circle.java
+++ src/Elmira/exampleCircleClass/Circle.java
@@ -2,33 +2,33 @@ package src.Elmira.exampleCircleClass;
 
 public class Circle {
 
-    double radious=1.0;
+    double radius;
 
     public Circle(){
-
+        this.radius=1.0;
     }
-    public Circle(double radious){
-        this.radious = radious;
+    public Circle(double radius){
+        this.radius = radius;
     }
-    public double getRadious() {
-        return radious;
+    public double getRadius() {
+        return radius;
     }
-    public void setRadious(double radious) {
-        this.radious = radious;
+    public void setRadius(double radius) {
+        this.radius = radius;
     }
     public double getArea(){
         //A=π*r*r
-        double A = Math.PI * Math.pow(this.radious,2);
+        double A = Math.PI * Math.pow(this.radius,2);
         return A;
     }
     public double getCircumference(){
         //C=2πr
-        double C = 2*this.radious*Math.PI;
+        double C = 2*this.radius*Math.PI;
         return C;
     }
     @Override
     public String toString() {
-        String str = "Circle[radious="+this.radious+"]";
+        String str = "Circle[radius="+this.radius+"]";
         return str;
     }
 }
