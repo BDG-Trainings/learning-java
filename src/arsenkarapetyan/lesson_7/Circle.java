68a8734ded85ea9bdc2859e17a148fb11251d74c
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-01-18 18:31:01 +0400

    lesson 7

---
+++ src/arsenkarapetyan/lesson_7/Circle.java
@@ -0,0 +1,26 @@
+package arsenkarapetyan.lesson_7;
+
+public class Circle {
+
+    static double radius=1.0;
+    static String color = "red";
+
+    public Circle(){
+    }
+
+    public Circle(double radius){
+    }
+
+    public Circle(double radius, String color){
+    }
+
+    public double getRadius (){
+        return radius;
+    }
+
+    public String getColor (){
+        return color;
+    }
+
+
+}
