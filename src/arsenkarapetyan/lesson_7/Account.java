8972271c998a1a5afad6fcab5ea5dfaffe937f72
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-01-18 12:07:18 +0400

    lesson 5

---
+++ src/arsenkarapetyan/lesson_7/Account.java
@@ -0,0 +1,14 @@
+package arsenkarapetyan.lesson_7;
+
+public class Account {
+
+    static String id;
+    static String name;
+    static int balance = 0;
+
+    public Account (String id, String name){
+
+    }
+
+
+}
