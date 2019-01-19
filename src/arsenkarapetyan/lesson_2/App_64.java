70227df7a7b70ab957621acb1aa6b81d155e67a6
ArsenKarapetyan <arsen.karapetyansam@gmail.com>
2019-01-19 19:06:18 +0400

    update

--- src/arsenkarapetyan/lesson_2/App_64.java
+++ src/arsenkarapetyan/lesson_2/App_64.java
@@ -1,19 +1,5 @@
 package arsenkarapetyan.lesson_2;
 
 public class App_64 {
-    public static void main(String[] args) {
-        int a = 4124;
 
-        int haz = (a / 1000);
-        int mia = (a % 10);
-        if (haz == 4 && mia == 4){
-
-            System.out.println("YES");
-        }
-
-        else {
-            System.out.println("NO");
-        }
-
-    }
 }
