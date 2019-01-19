4aba6326075aeeef24a4bdfb38458ded44668a96
ArsenKarapetyan <arsen.karapetyansam@gmail.com>
2019-01-19 19:11:47 +0400

    update

--- src/arsenkarapetyan/lesson_2/App_67.java
+++ src/arsenkarapetyan/lesson_2/App_67.java
@@ -5,16 +5,17 @@ public class App_67 {
     public static void main(String[] args) {
         int a = 4124;
 
-        int haz = (a / 1000);
-        int mia = (a % 10);
-        if (haz == 4 && mia == 4){
+        double haz = (a / 1000);
+        double har = ((a % 1000) / 100);
+        double tas = ((a % 100) / 10);
+        double mia = (a % 10);
 
+        if (a == Math.pow(2, haz+har+tas+mia)){
             System.out.println("YES");
         }
-
         else {
             System.out.println("NO");
         }
-
+        System.out.println(Math.pow(haz+har+tas+mia, 2));
     }
 }
