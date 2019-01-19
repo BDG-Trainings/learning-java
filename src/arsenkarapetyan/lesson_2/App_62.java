c1289abf55a71094279dffd28b5f94cb8079f1dd
ArsenKarapetyan <arsen.karapetyansam@gmail.com>
2019-01-19 19:01:42 +0400

    update

--- src/arsenkarapetyan/lesson_2/App_62.java
+++ src/arsenkarapetyan/lesson_2/App_62.java
@@ -4,17 +4,20 @@ public class App_62 {
     public static void main(String[] args) {
         int a = 4558;
         int b;
-
+        int c;
 
        if (a < 5000){
-
            b=a%10;
            System.out.println(b);
-
-
+           c=a%1000/100;
+           System.out.println(c);
        }
        else{
-
+           b = a/1000;
+           System.out.println(b);
+           c = a%100/10;
+           System.out.println(c);
        }
+       System.out.println(a/(b+c));
     }
 }
