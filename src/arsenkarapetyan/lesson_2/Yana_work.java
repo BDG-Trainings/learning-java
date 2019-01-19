b4c867792d4722d6a0207767b4cd6fbf27cde7c1
ArsenKarapetyan <arsen.karapetyansam@gmail.com>
2019-01-19 20:39:19 +0400

    update

--- src/arsenkarapetyan/lesson_2/Yana_work.java
+++ src/arsenkarapetyan/lesson_2/Yana_work.java
@@ -1,21 +1,37 @@
 package arsenkarapetyan.lesson_2;
 
 import java.util.Scanner;
+import java.util.jar.JarEntry;
 
 public class Yana_work {
 
     public static void main(String[] args) {
 
-        System.out.println("Яна, что ты получила?");
-        Scanner scan = new Scanner(System.in);
 
-        int ocenka = scan.nextInt();
+        double a = 5;
+
+        System.out.println(Math.pow(a,2));
+
+        /*
+
+        for (int i=1; i<6; i++){
+
+            sum=sum+i;
+            //System.out.println((i+1) + " I love the animals and plants");
+        }
+        System.out.println(sum);
+        System.out.println("Яна, что ты получила?");
+        System.out.println("Яна что ты получила?");
+        Scanner scan = new Scanner(System.);
+        int ocenka = scan.nextInt()
 
         if (ocenka >= 7){
             System.out.println("Apres!");
         }
         else {
-            System.out.println("Amot");
+            System.out.println("Amot!");
         }
+        */
+
     }
 }
