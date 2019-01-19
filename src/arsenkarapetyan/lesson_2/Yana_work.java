7fe5e7e45167863e2509a87ae29eece5a20ae678
ArsenKarapetyan <arsen.karapetyansam@gmail.com>
2019-01-19 20:14:38 +0400

    update

--- src/arsenkarapetyan/lesson_2/Yana_work.java
+++ src/arsenkarapetyan/lesson_2/Yana_work.java
@@ -1,10 +1,21 @@
 package arsenkarapetyan.lesson_2;
 
+import java.util.Scanner;
+
 public class Yana_work {
 
     public static void main(String[] args) {
 
+        System.out.println("Яна, что ты получила?");
+        Scanner scan = new Scanner(System.in);
+
+        int ocenka = scan.nextInt();
+
+        if (ocenka >= 7){
+            System.out.println("Apres!");
+        }
+        else {
+            System.out.println("Amot");
+        }
     }
-
-
 }
