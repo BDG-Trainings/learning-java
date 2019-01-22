b380f641af397105ca0cb0329c345dfe37277d59
Arsen Karapetyan <arsen.karapetyan@iunetworks.am>
2019-01-22 15:07:31 +0400

    lesson 7

--- src/arsenkarapetyan/lesson_7/Circle_Simplified.java
+++ src/arsenkarapetyan/lesson_7/Circle_Simplified.java
@@ -2,9 +2,10 @@ package arsenkarapetyan.lesson_7;
 
 public class Circle_Simplified {
 
-    private double radius=1.0;
+    private double radius;
 
     public Circle_Simplified(){
+        radius = 1.0;
     }
 
     public Circle_Simplified(double radius){
@@ -30,14 +31,18 @@ public class Circle_Simplified {
     }
 
     public String toString(){
-        String str = "Radius is equal: " + this.radius;
+        String str = "Radius is equal: " + getRadius() + ", Area is equal: " + getArea() + ", Circumference is equal: " + getCircumference();
         return str;
     }
 
     public static void main(String[] args) {
+
         Circle_Simplified cs = new Circle_Simplified(12);
         System.out.println(cs.toString());
-        System.out.println("Area is equal: " + cs.getArea());
-        System.out.println("Circumference is equal: " + cs.getCircumference());
+
+        System.out.println();
+
+        Circle_Simplified cs1 = new Circle_Simplified();
+        System.out.println(cs1.toString());
     }
 }
